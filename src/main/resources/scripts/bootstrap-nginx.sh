247c046ba5c0ddbbbaccf21b8c595b7f20c001c9
hokiegeek2 <hokiegeek2@gmail.com>
2016-08-25 15:55:02 -0400

    Fixed conf file generation bugs

--- src/main/resources/scripts/bootstrap-nginx.sh
+++ src/main/resources/scripts/bootstrap-nginx.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-python -c 'from marathon.nginx import nginx_resource_management; nginx_resource_management.writeConfFile()'
 
-exec nginx -g daemon off
+python -c 'from marathon.nginx import nginx_resource_management; nginx_resource_management.writeConfFile()'
+echo "configuration file written, launching nginx"
+nginx -g "daemon off;"
+echo "launched nginx"
