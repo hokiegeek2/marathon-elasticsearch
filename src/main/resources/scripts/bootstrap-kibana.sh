a7604f45996c305d50d258045a9b7de710a7d964
hokiegeek2 <hokiegeek2@gmail.com>
2016-09-19 10:42:43 -0400

    First version for testing

--- src/main/resources/scripts/bootstrap-kibana.sh
+++ src/main/resources/scripts/bootstrap-kibana.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-host=$(python -c 'from marathon.elasticsearch import nginx_resource_management; nginx_resource_management.getNginxHost; \
+es_url=$(python -c 'from marathon.elasticsearch import nginx_resource_management; nginx_resource_management.getNginxHost; \
      host = resource_management.getHost(); print(host)')
      
-export NGINX_HOST=host
-
-echo $NGINX_HOST
+/opt/kibana/bin/kibana --elasticsearch.url=$es_url
