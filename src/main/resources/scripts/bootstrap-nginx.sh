1f8a82253000d963434068c1a81a39a2def26c1a
root <root@ip-172-31-2-167.eu-central-1.compute.internal>
2016-09-19 13:51:10 +0000

    fixed password protect implementation

--- src/main/resources/scripts/bootstrap-nginx.sh
+++ src/main/resources/scripts/bootstrap-nginx.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+i#!/bin/bash
+
+htpasswd -c -b /etc/nginx/.htpasswd $USERNAME $PASSWORD
 
 python -c 'from marathon.nginx import nginx_resource_management; nginx_resource_management.writeConfFile()'
 echo "configuration file written, launching nginx"
